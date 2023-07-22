af19e12388ab1b045e957a08c29b0272e6c56ad9
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-22 15:55:18 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -2,10 +2,32 @@
   description = "Ripxorips NixOS flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  };
+    # You can access packages and modules from different nixpkgs revs at the
+    # same time. See 'unstable-packages' overlay in 'overlays/default.nix'.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-formatter-pack.url = "github:Gerschtli/nix-formatter-pack";
+    nix-formatter-pack.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+  outputs =
+    { self
+    , nix-formatter-pack
+    , nixpkgs
+    , ...
+    } @ inputs:
     let
+      inherit (self) outputs;
+      # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+      stateVersion = "23.05";
+      libx = import ./lib { inherit inputs outputs stateVersion; };
       system = "x86_64-linux";
 
       pkgs = import.nixpkgs {
@@ -19,6 +41,7 @@
     {
 
       nixosConfigurations = {
+        ripxowork = libx.mkHost { hostname = "ripxowork"; username = "ripxorip"; desktop = "gnome"; };
         ripxonix = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit system;};
           modules = [
