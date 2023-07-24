68992007f6e52c222a7f8e68377b3e19f6661fdb
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-24 09:39:17 +0200

    WIP

--- home-manager/_mixins/console/starship.nix
+++ home-manager/_mixins/console/starship.nix
@@ -2,7 +2,24 @@
   programs.starship = {
     enable = true;
     settings = {
-      add_newline = true;
+
+    add_newline = true;
+
+    directory = {
+      truncation_length = 8;
+      truncate_to_repo = false;
+      truncation_symbol = "…/";
+    };
+
+    hostname = {
+      ssh_only = false;
+    };
+
+    status = {
+      symbol = "";
+      disabled=false;
+    };
+
     };
   };
 }
