964acf62cf144119083db274279d1194e5cf3bfd
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-24 10:19:33 +0200

    Adds starship manually

--- home-manager/_mixins/console/zsh.nix
+++ home-manager/_mixins/console/zsh.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }: {
   programs.zsh = {
     enable = true;
+    initExtra = ''
+      # Continue here with oh-my-zsh plugins, etc.
+      eval "$(starship init zsh)"
+    '';
   };
 }
