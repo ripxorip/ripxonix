6159d008ff2878f6f1f607794bb47f71637357d0
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-25 07:47:47 +0200

    Inc

--- home-manager/_mixins/console/git.nix
+++ home-manager/_mixins/console/git.nix
@@ -1,7 +1,32 @@
 { config, lib, pkgs, ... }: {
   programs = {
     git = {
-
+      enable = true;
+      delta = {
+        enable = true;
+        options = {
+          features = "decorations";
+          navigate = true;
+          side-by-side = true;
+        };
+      };
+      aliases = {
+        adog = "log --all --decorate --oneline --graph";
+        co = "checkout";
+      };
+      userEmail = "ripxorip@gmail.com";
+      userName = "Philip K. Gisslow";
+      extraConfig = {
+        push = {
+          default = "matching";
+        };
+        pull = {
+          rebase = true;
+        };
+        init = {
+          defaultBranch = "master";
+        };
+      };
     };
   };
 }
