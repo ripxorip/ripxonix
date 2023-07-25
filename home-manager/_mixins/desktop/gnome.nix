3f49c45c95fb94dc5b2d78b2ec0b5a25fcfe4aed
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-25 17:08:57 +0200

    Inc

--- home-manager/_mixins/desktop/gnome.nix
+++ home-manager/_mixins/desktop/gnome.nix
@@ -1,7 +1,12 @@
 { lib, ... }:
 with lib.hm.gvariant;
 {
-  dconf.settings = { };
+  dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      xkb-options=["eurosign:e" "caps:ctrl_modifier"];
+      sources=[ (mkTuple["xkb" "us+colemak_dh"])];
+    };
+  };
 
   gtk = { };
 
