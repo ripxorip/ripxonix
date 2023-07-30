5961a0727d31548e90efbb06b6d84027e4eea91f
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-30 19:53:10 +0200

    Systemd test

---
+++ home-manager/_mixins/users/ripxorip/hosts/ripxonode.nix
@@ -0,0 +1,23 @@
+{ lib, ... }:
+{
+  imports = [ ];
+
+  systemd.timers."hello-world" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "5m";
+      Unit = "hello-world.service";
+    };
+  };
+
+  systemd.services."hello-world" = {
+    script = ''
+      set -eu
+      ${pkgs.coreutils}/bin/echo "Hello World"
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+  }
