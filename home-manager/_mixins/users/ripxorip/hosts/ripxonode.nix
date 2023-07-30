f852b69b31148b7e659b0bd1006d9f1c087e71bd
Philip K. Gisslow <ripxorip@gmail.com>
2023-07-30 20:15:59 +0200

    Systemd test revert

--- home-manager/_mixins/users/ripxorip/hosts/ripxonode.nix
+++ home-manager/_mixins/users/ripxorip/hosts/ripxonode.nix
@@ -1,24 +1,4 @@
 { lib, pkgs, ... }:
 {
   imports = [ ];
-
-  systemd.user.timers."hello-world" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitActiveSec = "5m";
-      Unit = "hello-world.service";
-    };
-  };
-
-  systemd.user.services."hello-world" = {
-    script = ''
-      set -eu
-      ${pkgs.coreutils}/bin/echo "Hello World"
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-    };
-  };
 }
