d1b11b843fce490c94178f0884537e75e19c97d0
Philip K. Gisslow <ripxorip@gmail.com>
2023-08-03 07:51:28 +0200

    Adds docker workaround

--- nixos/_mixins/services/syncthing.nix
+++ nixos/_mixins/services/syncthing.nix
@@ -1,8 +1,8 @@
 { hostname, pkgs, username, ... }: {
-services.syncthing = {
-        enable = true;
-        user = "${username}";
-        dataDir = "/home/${username}/Documents"; # Default folder for new synced folders
-        configDir = "/home/${username}/.config/syncthing";
-    };
+  services.syncthing = {
+    enable = true;
+    user = "${username}";
+    dataDir = "/home/${username}/Documents"; # Default folder for new synced folders
+    configDir = "/home/${username}/.config/syncthing";
+  };
 }
--- nixos/_mixins/virt/default.nix
+++ nixos/_mixins/virt/default.nix
@@ -8,6 +8,11 @@
   virtualisation = {
     docker = {
       enable = true;
+      # FIXME Workaround until docker is updated
+      # Will need to be removed in the near future
+      package = pkgs.docker.override {
+        buildGoPackage = pkgs.buildGo118Package;
+      };
     };
     libvirtd = {
       enable = true;
