330d797dabcdabcf786da851b41825559d55b103
Philip K. Gisslow <ripxorip@gmail.com>
2023-10-08 15:57:34 +0200

    feat: Adds cockpit for lab

---
+++ nixos/_mixins/services/cockpit.nix
@@ -0,0 +1,3 @@
+_: {
+  services.cockpit.enable = true;
+}
--- nixos/ripxolab/default.nix
+++ nixos/ripxolab/default.nix
@@ -4,6 +4,7 @@
 {
   imports = [
     ../_mixins/services/tailscale.nix
+    ../_mixins/services/cockpit.nix
     ../_mixins/services/caddy.nix
     ../_mixins/services/syncthing.nix
     ../_mixins/services/jellyfin.nix
