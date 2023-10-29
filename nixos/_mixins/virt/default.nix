534cda4d69af01c45a54cf23cccb1cdab753395b
Philip K. Gisslow <ripxorip@gmail.com>
2023-10-29 10:40:25 +0100

    wip: TPM and Win11 VM support

--- nixos/_mixins/virt/default.nix
+++ nixos/_mixins/virt/default.nix
@@ -1,4 +1,4 @@
-{ desktop, lib, pkgs, ... }: {
+{ desktop, lib, pkgs, config, ... }: {
   imports = [ ] ++ lib.optional (builtins.isString desktop) ./desktop.nix;
 
   #https://nixos.wiki/wiki/Podman
@@ -9,9 +9,25 @@
     docker = {
       enable = true;
     };
+
     libvirtd = {
       enable = true;
-      qemu.swtpm.enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+        swtpm.enable = true;
+      };
+    };
+  };
+
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
+
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
     };
   };
 }
