9e8d0ae34b0428de87087fabafc2a4e44cd6bc6e
Philip K. Gisslow <ripxorip@gmail.com>
2023-12-02 07:42:28 +0100

    fix: Renaming of ripxospeech script

--- home-manager/_mixins/desktop/gnome.nix
+++ home-manager/_mixins/desktop/gnome.nix
@@ -34,31 +34,31 @@ with lib.hm.gvariant;
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = lib.hm.gvariant.mkString "<Shift>F9";
-      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/scripts/run.py -a start_dictation -e win11_swe";
+      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/ripxospeech.py -a start_dictation -e win11_swe";
       name = lib.hm.gvariant.mkString "Ripxospeech Stop";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       binding = lib.hm.gvariant.mkString "<Shift>F10";
-      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/scripts/run.py -a start_dictation -e talon_dictation";
+      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/ripxospeech.py -a start_dictation -e talon_dictation";
       name = lib.hm.gvariant.mkString "Ripxospeech Stop";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
       binding = lib.hm.gvariant.mkString "<Shift>F11";
-      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/scripts/run.py -a start_dictation -e talon_command";
+      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/ripxospeech.py -a start_dictation -e talon_command";
       name = lib.hm.gvariant.mkString "Ripxospeech Stop";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
       binding = lib.hm.gvariant.mkString "<Shift>F12";
-      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/scripts/run.py -a stop_dictation";
+      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/ripxospeech.py -a stop_dictation";
       name = lib.hm.gvariant.mkString "Ripxospeech Start";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
       binding = lib.hm.gvariant.mkString "<Shift>F8";
-      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/scripts/run.py -a start_dictation -e gdocs";
+      command = lib.hm.gvariant.mkString "/home/ripxorip/dev/ripxospeech/ripxospeech.py -a start_dictation -e gdocs";
       name = lib.hm.gvariant.mkString "Ripxospeech Start";
     };
   };
