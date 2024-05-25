1e14a0a7657ce81044a8564aa22bc8e5d6b9d827
Philip K. Gisslow <ripxorip@gmail.com>
2024-05-25 08:07:15 +0200

    feat: Network bridge for Odroid

--- nixos/ripxodroid/default.nix
+++ nixos/ripxodroid/default.nix
@@ -40,18 +40,20 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
 
-  # FIXME Setup properly for the odroid
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
+  networking.useDHCP = false;
+  networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+
+  networking.bridges.br0.interfaces = [ "enp1s0" ];
+  networking.interfaces.br0 = {
+    useDHCP = false;
+    ipv4.addresses = [{
+      "address" = "10.0.0.212";
+      "prefixLength" = 24;
+    }];
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
