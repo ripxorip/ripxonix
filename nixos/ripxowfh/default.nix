d34b2d0b832ea762b9da9665ac08b83ed623e5c5
Philip K. Gisslow <ripxorip@gmail.com>
2024-07-04 13:28:19 +0200

    feat: Adb for wfh

--- nixos/ripxowfh/default.nix
+++ nixos/ripxowfh/default.nix
@@ -20,6 +20,7 @@
 
   # See https://github.com/Mic92/envfs (for scripts to get access to /bin/bash etc.)
   services.envfs.enable = true;
+  programs.adb.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
