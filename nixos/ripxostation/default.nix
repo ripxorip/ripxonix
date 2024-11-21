6fac1695cc144e0b7ab5843d5d7a811a154476cc
Philip K. Gisslow <ripxorip@gmail.com>
2024-11-21 08:43:49 +0100

    feat: Station now runs frigate

--- nixos/ripxostation/default.nix
+++ nixos/ripxostation/default.nix
@@ -163,6 +163,12 @@
     openFirewall = true;
   };
 
+  fileSystems."/mnt/nvr" =
+    {
+      device = "/dev/disk/by-uuid/fb93ec8f-2c8c-4504-aad8-7d0e9123e34c";
+      fsType = "ext4";
+    };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
